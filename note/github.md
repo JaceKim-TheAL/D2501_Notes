52cc3a4bbc239b74fe48f77aab623071a32db165
Jace Kim <jacekim.theal@google.com>
2025-04-25 11:30:10 +0900

    git test in vscode

---
+++ note/github.md
@@ -0,0 +1,6 @@
+# GitHub & VSCode 연동
+
+- GitHub 설치
+- VSCode의 Extension 설치
+
+
--- script.js
+++ script.js
@@ -1,1 +1,5 @@
 console.log("How to use GitHub and VSCode!");
+
+function showAlert() {
+    alert("Hello, this is a test alert!");
+}   
