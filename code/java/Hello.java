074474d77f7b19d9ad4bdec66a4dba7256351ee2
Jace Kim <jacekim.theal@gmail.com>
2025-05-25 12:39:44 +0900

    Practice Code

---
+++ code/java/Hello.java
@@ -0,0 +1,10 @@
+public class Hello {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+
+    public static String greet(String name) {
+        return "Hello, " + name + "!";
+    }   
+}
