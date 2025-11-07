f6c7f4cd3f7e1e387b09b80d3a8db15ccd7e985f
Jace Kim <jacekim.theal@gmail.com>
2025-11-08 01:10:23 +0900

    .\.git\

---
+++ Q-Net/html/terms/common/app_tab_menu.js
@@ -0,0 +1,39 @@
+termskeywordtab_url_list = [
+  'https://htmlpreview.github.io/?https://raw.githubusercontent.com/JaceKim-TheAL/D2501_Notes/master/Q-Net/html/terms/keyword.html', 
+  'https://htmlpreview.github.io/?https://raw.githubusercontent.com/JaceKim-TheAL/D2501_Notes/master/Q-Net/html/terms/keyword1.html', 
+  'https://htmlpreview.github.io/?https://raw.githubusercontent.com/JaceKim-TheAL/D2501_Notes/master/Q-Net/html/terms/keyword2.html', 
+  'https://htmlpreview.github.io/?https://raw.githubusercontent.com/JaceKim-TheAL/D2501_Notes/master/Q-Net/html/terms/keyword3.html', 
+  'https://htmlpreview.github.io/?https://raw.githubusercontent.com/JaceKim-TheAL/D2501_Notes/master/Q-Net/html/terms/keyword4.html', 
+  'https://htmlpreview.github.io/?https://raw.githubusercontent.com/JaceKim-TheAL/D2501_Notes/master/Q-Net/html/terms/keyword5.html' 
+];
+
+tab_file_list = [
+  './keyword.html',
+  './keyword1.html',
+  './keyword2.html',
+  './keyword3.html',
+  './keyword4.html',
+  './keyword5.html'
+];
+
+
+  function navigateToTab(index) {
+    // window.location.href = tab_url_list[index];
+    window.location.href = tab_file_list[index];
+  }
+
+  function showChapter(index) {
+    const contents = document.querySelectorAll('.tab-content');
+    const buttons = document.querySelectorAll('.tab-header button');
+
+    contents.forEach((c, i) => {
+      c.classList.toggle('active', i === index);
+    });
+
+    buttons.forEach((b, i) => {
+      b.classList.toggle('active', i === index);
+    });
+  }
+
+  
+  
